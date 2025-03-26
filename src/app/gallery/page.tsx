a1cde31123c6eb111eb07ac270a8541b9eebfed5
smarterToby <84527857+smarterToby@users.noreply.github.com>
2025-03-26 19:18:12 +0100

    fix: type + build errors

--- src/app/gallery/page.tsx
+++ src/app/gallery/page.tsx
@@ -45,7 +45,7 @@ export default function Gallery() {
             name: gallery.title,
             description: gallery.description,
             url: `https://${baseURL}/gallery`,
-            image: gallery.images.map((image) => ({
+            image: gallery.images.map((image: any) => ({
               "@type": "ImageObject",
               url: `${baseURL}${image.src}`,
               description: image.alt,
